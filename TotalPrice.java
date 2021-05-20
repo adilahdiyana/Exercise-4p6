46d89473aac8a29d9ead9b43d8e065b1bdaa061f
adilahdiyana <81138230+adilahdiyana@users.noreply.github.com>
2021-05-20 12:20:03 +0800

    Create TotalPrice.java

---
+++ TotalPrice.java
@@ -0,0 +1,10 @@
+package Exercise4_6;
+
+interface TotalPrice {
+	
+	double price(); //abstract method with no implementation or no body
+	double price2(); //abstract method with no implementation or no body
+	double totalPrice(int quantity); //abstract method with no implementation or no body
+	double totalPrice(int quantity, double disc); //abstract method with no implementation or no body
+
+} //end class of TotalPrice
